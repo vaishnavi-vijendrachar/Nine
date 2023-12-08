5f5befa8caf5c08489fc3bc00ec90bdb3e443954
Vaishnavi <vaishnavi.vijendrachar@gmail.com>
2023-12-08 16:45:06 +1100

    commit

---
+++ app/src/main/java/com/vaishnavi/nine/Sample3.kt
@@ -0,0 +1,49 @@
+package com.vaishnavi.nine
+
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.vaishnavi.nine.R
+
+@Composable
+fun Sample3() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(id = R.color.background_primary))
+            .padding(dimensionResource(id = R.dimen.padding_3x)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            stringResource(id = R.string.error_message_warning),
+            color = colorResource(id = R.color.font),
+            fontSize = 20.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.ExtraLight
+        )
+        Spacer(modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_1x)))
+        Text(
+            stringResource(id = R.string.error_message_action),
+            color = colorResource(id = R.color.font),
+            fontSize = 18.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.ExtraLight
+        )
+    }
+}
